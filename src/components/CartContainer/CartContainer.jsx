0b10adbc2e961966dbd768a30af49566eb771f25
Hans <you@example.com>
2022-11-04 13:28:26 +0100

    subida con cupon funcionando

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -6,32 +6,38 @@ import './CartContainer.css'
 import { useContext,useState } from "react"
 import { CartContext } from "../../context/CartContext"
 import { db } from '../../utils/firebase'
-import { collection, addDoc, doc, getDoc } from 'firebase/firestore'
+import { collection, addDoc } from 'firebase/firestore'
+import { Link } from 'react-router-dom';
 
 
 export const CartContainer = ()=>{
 
+     // Muestra Productos
+     const value = useContext(CartContext);
+
+     const {prodCarrito, getTotalPrice, getTotalProducts,removeProduct, clear} = value;
+
+
      // Cupon descuento
+
+     const [newPrice, setNewPrice]= useState(0)
 
     const [cuponDto, setCuponDto] = useState('');
 
-     const aplicaCupon = (cupon)=>{
-        cupon.preventDefault()
-        console.log(cupon)
-        setCuponDto(cupon);
-        
+     const aplicaCupon = (e)=>{
+        e.preventDefault(); 
         if (cuponDto === 'Lan022') {
 
         const priceCuponDto = getTotalPrice() - (getTotalPrice() * 0.05);
-        priceCuponDto = getTotalPrice();
+        setNewPrice(priceCuponDto)
         console.log(priceCuponDto)
         
         } else if (cuponDto == '') {
             console.log('puede introducir un cupon')
-
+            
         } else {
 
-         console.log('puede introducir un cupon')
+         console.log('Este cupon no tiene descuento')
         }
     }
 
@@ -39,26 +45,30 @@ export const CartContainer = ()=>{
     
     const Msg = () => (
         <div>
+          <p>Muchas Gracias</p>
           {compraId && <p>Su compra fue realizada con el numero de orden {compraId}</p>}
-          
-         
+          <Link to="/"><button className='mt-2 btn btn-primary' onClick={clear()}>Realizar Otra Compra</button></Link>
         </div>
       )
 
     const displayMsg = () => {
-        toast(<Msg />) 
-
+        toast(<Msg />, {position: "top-right",
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+            }) 
       }
 
       const [compraId, setCompraId] = useState('');
 
-    // Muestra Productos
-    const value = useContext(CartContext);
 
-    const {prodCarrito, getTotalPrice, getTotalProducts,removeProduct} = value;
-
-    
     // Mandar Orden de Compra
+
+
     const SendOrder = (evt)=>{
         evt.preventDefault(); 
         const compra = {
@@ -70,37 +80,16 @@ export const CartContainer = ()=>{
             items: prodCarrito,
             total: getTotalPrice()
         }  
-      
         
         // Crear y subir a Firestore
         const queryRef = collection(db,'orders')
       
         addDoc(queryRef,compra).then((resultado)=>{
-            console.log(resultado.id)
             setCompraId(resultado.id)
+           
         })
 
-
-       
-
-        }
-
-        
-        // const getName = async()=>{
-
-        //     const docRef = doc(db,"orders",compra.buyer);
-        //     const docSnap = await getDoc(docRef);
-
-        //     if (docSnap.exists()) {
-        //     console.log("Document data:", docSnap.data());
-        //     } else {
-        //     console.log("No such document!");
-        //     }  
-
-        // }
-        // getName(); 
-        
-
+    }
     
 
     return(
@@ -139,27 +128,33 @@ export const CartContainer = ()=>{
                 <p><strong>Productos: </strong>{getTotalProducts()}</p>
                 <p><strong>Finalizar Compra</strong></p>
 
-                <form className='aplicaCupon d-block' onSubmit={aplicaCupon}>
-                <label className='mx-3'>Cupón Dto del 5%</label>
-                    <input type='text' placeholder="Aplica Cupón Dto"/>
-                    <button type="Submit" className='d-block mt-2 mx-5 btn btn-dark'>Aplicar</button>
+                <form className='aplicaCupon d-block' >
+                <label>Cupón Dto del 5%</label>
+                    <input type='text' placeholder="Aplica Cupón Dto" onChange={(e)=>setCuponDto(e.target.value)}/>
+                    <button type="submit" onClick={aplicaCupon} className='mt-2 mx-5 btn btn-dark'>Aplicar</button>
                 </form>
 
-                <p>Precio Final: {getTotalPrice()}</p>
+                <p>Precio Final:</p> 
+                    {
+                    newPrice > 0 ? 
+                    <div><p className='precioTachado'>{getTotalPrice()}</p> <p><stong>{newPrice}</stong></p>
+                    </div> : <p>{getTotalPrice()}</p>
+                }
+
 
                 <form className='formCompra mt-3 mr-3' required= "required" onSubmit={SendOrder}>
                     <label>Nombre</label>
-                    <input type='text' placeholder="Nombre" required maxlength="45"/>
+                    <input   type='text' placeholder="Nombre"  maxLength="45"/>
                     <label>Apellidos</label>
-                    <input type='text' placeholder="Apellidos" required maxlength="45"/>
+                    <input   type='text' placeholder="Apellidos"  maxLength="45"/>
                     <label>Dirección</label>
-                    <input type='text' placeholder="Dirección" required maxlength="100" />
+                    <input   type='text' placeholder="Dirección"  maxLength="100" />
                     <label>Codigo Postal</label>
-                    <input type='number' required maxlenght='5' placeholder="Código Postal"/>
+                    <input   type='number' maxLength='5' placeholder="Código Postal"/>
                     <label>Teléfono</label>
-                    <input type='tel' required maxlength="11" placeholder="Teléfono"/>
+                    <input   type='tel' maxLength="11" placeholder="Teléfono"/>
                     <label>Correo</label>
-                    <input type='mail' placeholder="Correo Electrónico"/>
+                    <input   type='mail' placeholder="Correo Electrónico"/>
                     <button className='d-block mt-5 mx-5 btn btn-dark text-center' type="Submit" onClick={displayMsg}>Comprar</button>
                 </form>
                 <ToastContainer />
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,20 +4,20 @@ import React, {useState, createContext} from "react"
 export const CartContext = createContext()
 
 export const CartProvider = ({children})=>{
-// creamos unn componente que retorna un provider de este contexto
+
     const [prodCarrito, setProdCarrito] = useState([]);
-    //verificamos si el producto ya existe o no
+
+    
     const isInCart = (id)=>{
-        // podriasmo usar filte o finde, el que mas lo hace es some
-        // si uno de los elemento cumple una condición me retorna un true
+        
         const prodExist = prodCarrito.some((elemento)=>elemento.id === id);
-        // some va a buscar todos los elementos si uno solo cumple la condición, some me retorna true
+       
         return prodExist;
     }
     
     const addProduct = (item, quantity)=>{
         const productosCartCopy = [...prodCarrito];
-        //si el producto ya existe, modificamos quantity
+        
         const newProduct= {
             ...item, 
             quantity: quantity,
@@ -32,28 +32,20 @@ export const CartProvider = ({children})=>{
 
             setProdCarrito(productosCartCopy);
         } else {
-         // sino agrego el nuevo producto al arreglo
+        
          
         productosCartCopy.push(newProduct)
         setProdCarrito(productosCartCopy)
         }
-        // item.title item.price... en vez de eso ...item spread operator
-       
-        //se suele usar una copia para no trabajar con la variable e estado
-        
-       
+      
+    
     }
 
     const getTotalPrice = ()=>{
-        // reduce es un metodo que nos permite ir acumulando valores en una variable, y esa variable es el
-        //valor de esta operación, la forma tradicional sera un for
-        /*
-        for (let i=0; i<array.lenght; i++) {
-            acc = acc + array[i].quantityPrice
-        }*/
+      
 
         const precioTotal = prodCarrito.reduce((acc,curr)=>acc + curr.quantityPrice,0)
-        // acumulaodr y curr current Price
+        
         return precioTotal;
     }
 
@@ -67,9 +59,14 @@ export const CartProvider = ({children})=>{
         setProdCarrito(newProducts)
 
     }
+
+    const clear = () => {
+        setProdCarrito([])
+    }
+
     return(
 
-        <CartContext.Provider value={{prodCarrito, addProduct, getTotalPrice, getTotalProducts, removeProduct}}>
+        <CartContext.Provider value={{prodCarrito, addProduct, getTotalPrice, getTotalProducts, removeProduct, clear}}>
             {children} 
         </CartContext.Provider>
 
